098c9bbfe37a170e656a4af277e553951b307da4
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2021-05-12 09:30:18 +0300

    Adding Collectables

---
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Project.Collectables
+{
+    public class Collectable : MonoBehaviour
+    {
+        [SerializeField] private int value;
+        [SerializeField] private Animator animator;
+        [SerializeField] private string collisionAnimationName;
+
+        private bool destroyed;
+
+        public virtual float OnPlayerCollided()
+        {
+            if (!destroyed)
+            {
+                destroyed = true;
+                PlayCollisionAnimation();
+                return value;
+            }
+            return -1;
+        }
+        protected void PlayCollisionAnimation()
+        {
+            animator.SetTrigger(collisionAnimationName);
+        }
+    }
+}
